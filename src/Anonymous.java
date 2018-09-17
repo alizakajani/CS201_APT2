c7494502a11353a7985f748c5595a85c833e2921
Aliza Kajani <aliza.kajani@gmail.com>
2018-09-17 19:33:50 -0400

    IsomorphicWords complete

--- src/Anonymous.java
+++ src/Anonymous.java
@@ -1,30 +1,34 @@
 
 public class Anonymous {
 	public int howMany(String[] headlines, String[] messages) {
-        
-		int[] hCount = new int[26];
-        
+        int total = 0;
+        boolean a = true;
+		int[] counts = new int[256];
 		for(String s : headlines) {
-			s = s.toLowerCase();
-			for(char ch : s.toCharArray()) {
-				hCount[ch] += 1;
+			for(char ch : s.toLowerCase().toCharArray()) {
+				if(ch == ' ') continue;
+				counts[ch] += 1;
 			}
 		}
 		
 		for(String m : messages) {
-			m = m.toLowerCase();
-			int[] mCount = new int[26];
-			boolean a = true;
-			for(char ch : m.toCharArray()) {
-				mCount[ch] += 1;
+			int[] mcounts = new int[256];
+			a = true;
+			for(char ch : m.toLowerCase().toCharArray()) {
+				if(ch == ' ') continue;
+				mcounts[ch] += 1;
 			}
-			for(int k = 0; k < 256; k++) {
-				if(mCount[k] > hCount[k]) {
+			for(char ch : m.toLowerCase().toCharArray()) {
+				if(mcounts[ch] <= counts[ch]) continue;
+				else if(mcounts[ch] > counts[ch]) {
 					a = false;
 				}
 			}
+			if(a == true) {
+				total += 1;
+			}
 		}
 		
-        return 0;
+        return total;
   }
 }
--- src/IsomorphicWords.java
+++ src/IsomorphicWords.java
@@ -1,6 +1,38 @@
+import java.util.*;
+
 
 public class IsomorphicWords {
+	
 	public int countPairs(String[] words) {
-        // fill in code here
+        
+		int total = 0;
+        
+        for(int j = 0; j < words.length; j += 1) {
+        	String jword = iso(words[j]);
+        	for(int k = j + 1; k < words.length; k += 1) {
+        		String kword = iso(words[k]);
+        		if(jword.equals(kword)) {
+        			total += 1;
+        		}
+        	}
+        }
+        return total;
      }
+	
+	char[] dict = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	private String iso(String word) {
+		char[] chars = word.toCharArray();
+		HashMap<Character, Character> charMap = new HashMap<>();
+		for(int k = 0; k < word.length(); k += 1) {
+			if(! charMap.containsKey(chars[k])) {
+				charMap.put(chars[k], dict[k]);
+			}
+			if(charMap.containsKey(chars[k])) {
+				chars[k] = charMap.get(chars[k]);
+			}
+		}
+		String isoWord = new String(chars);
+		return isoWord;
+	}
+
 }
