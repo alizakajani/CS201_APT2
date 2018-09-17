57c3ea34599cab3571e155124663e7740e64b2a6
Aliza Kajani <aliza.kajani@gmail.com>
2018-09-17 10:47:13 -0400

    MemberCheck complete

--- src/MemberCheck.java
+++ src/MemberCheck.java
@@ -1,7 +1,26 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MemberCheck {
+	
 	public String[] whosDishonest(String[] club1, String[] club2, String[] club3) {
+		Set<String> result = new HashSet<>();
+		Set<String> s1 = new HashSet<>(Arrays.asList(club1));
+		Set<String> s2 = new HashSet<>(Arrays.asList(club2));
+		Set<String> s3 = new HashSet<>(Arrays.asList(club3));
 		
+		result.addAll(intersection(s1,s2));
+		result.addAll(intersection(s1,s3));
+		result.addAll(intersection(s2,s3));
+		String[] ret = result.toArray(new String[0]);
+		Arrays.sort(ret);
+		return ret;
 	}
-
+	
+	private Set<String> intersection(Set<String> string1, Set<String> string2) {
+		Set<String> intersection = new HashSet<>(string1);
+		intersection.retainAll(string2);
+		return intersection;
+	}
 }
